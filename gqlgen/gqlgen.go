661dc7a2eaa8fee80dec2d7e6d985586e9cfb3bc
Filip Wojciechowski <fwojciec@gmail.com>
2020-03-23 14:15:03 +0100

    custom gql server config

--- gqlgen/gqlgen.go
+++ gqlgen/gqlgen.go
@@ -4,17 +4,28 @@ import (
 	"net/http"
 
 	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/handler/extension"
+	"github.com/99designs/gqlgen/graphql/handler/lru"
+	"github.com/99designs/gqlgen/graphql/handler/transport"
 	"github.com/99designs/gqlgen/graphql/playground"
 )
 
 // NewQueryHandler returns a new GraphQL query handler.
 func NewQueryHandler(resolver *Resolver) http.Handler {
-	return handler.NewDefaultServer(NewExecutableSchema(Config{
+	srv := handler.New(NewExecutableSchema(Config{
 		Resolvers: resolver,
 		Directives: DirectiveRoot{
 			HasRole: HasRole(resolver.Session),
 		},
 	}))
+	srv.AddTransport(transport.Options{})
+	srv.AddTransport(transport.POST{})
+	srv.SetQueryCache(lru.New(1000))
+	srv.Use(extension.Introspection{})
+	srv.Use(extension.AutomaticPersistedQuery{
+		Cache: lru.New(100),
+	})
+	return srv
 }
 
 // NewPlaygroundHandler returns a new handler that serves GraphQL Playground.
